e78710e7858734782b8df2a2e417b2189f200c34
manishsharma1992 <mssharmam51@gmail.com>
2016-10-01 21:47:03 +0530

    Add files via upload

---
+++ start-jarvis.py
@@ -0,0 +1,39 @@
+import aiml
+import pyttsx
+import pyaudio
+import speech_recognition as sr
+import os
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
+
+#if os.path.isfile("bot_brain.brn"):
+#    kernel.bootstrap(brainFile = "bot_brain.brn")
+#else:
+#    kernel.resetBrain()
+#    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+#    kernel.saveBrain("bot_brain.brn")
+
+r = sr.Recognizer()
+engine = pyttsx.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-20)
+	
+# Press CTRL-C to break this loop
+while True:
+	print("Say something!")
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+	try:
+		input = r.recognize_google(audio)
+		print("You said : " + input)
+		response = kernel.respond(input)
+		print("Jarvis said: " + response)
+		engine.say(response)
+		engine.runAndWait()
+	except sr.UnknownValueError:
+		engine.say("I could not understand what you just said")
+		engine.runAndWait()
+	except sr.RequestError as e:
+		print("Could not request results".format(e))

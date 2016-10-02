2b4471a683a1b54d3bf80253353fba2b2b6304e3
manishsharma1992 <mssharmam51@gmail.com>
2016-10-02 12:02:12 +0530

    Update start-jarvis.py

--- start-jarvis.py
+++ start-jarvis.py
@@ -8,13 +8,6 @@ kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
 
-#if os.path.isfile("bot_brain.brn"):
-#    kernel.bootstrap(brainFile = "bot_brain.brn")
-#else:
-#    kernel.resetBrain()
-#    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-#    kernel.saveBrain("bot_brain.brn")
-
 r = sr.Recognizer()
 engine = pyttsx.init()
 rate = engine.getProperty('rate')
